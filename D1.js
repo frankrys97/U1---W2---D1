615ca6bd5bd8e03df3d5622be8d85b7fba2f7d1e
Francesco <frankrys97@gmail.com>
2024-03-04 14:57:26 +0100

    code indentation

--- D1.js
+++ D1.js
@@ -21,36 +21,33 @@ REGOLE
 // I principali elementi di questo linguaggio di programmazione sono le cosiddette variabili, ovvero dei "contenitori",
 // a livello di hardware corrisponde a delle celle nella RAM, all'interno dei quali è possibile inserire un valore, un dato.
 // I modi per definire una variabile sono principalmente tre: LET - CONST - VAR (in disuso).
-// I tipi di dato invece che questi "contenitori" possono ricevere al loro interno sono 5: 
+// I tipi di dato invece che questi "contenitori" possono ricevere al loro interno sono 5:
 
 // 1 - Numbers, ovvero proprio i numeri (interi, decimali, negativi), con i quali è possibile anche eseguire operazioni aritmetiche;
 
-    let num = 10
-    let num1 = 10.6
-    let num2 = -34
+let num = 10;
+let num1 = 10.6;
+let num2 = -34;
 
 // 2 - Stringhe, ovvero dei valori che corrispondono ad un testo, identificato dall'uso delle virgolette;
 
-    let name = "Susan"
-    let car = "Ford"
-
+let name = "Susan";
+let car = "Ford";
 
 // 3 - Boolean, ovvero un tipo di dato che accetta un valore true (vero) ed un valore false (false);
 
-    let isRaining = true
-    let isEasy = false
-
+let isRaining = true;
+let isEasy = false;
 
 // 4 - Undefined, ovvero un tipo di dato che indica qualcosa di non definito
 
-    let iDontKnow
+let iDontKnow;
 
-    console.log(iDontKnow);
-
+console.log(iDontKnow);
 
 // 5 - Null, ovvero un dato esplicitamente vuoto
 
-    let dog = null
+let dog = null;
 
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
@@ -58,7 +55,7 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
-const myName = "Francesco"
+const myName = "Francesco";
 
 console.log(myName);
 
@@ -68,9 +65,9 @@ console.log(myName);
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
-let number1 = 12
-let number2 = 20 
-let sumNum = number1 + number2
+let number1 = 12;
+let number2 = 20;
+let sumNum = number1 + number2;
 
 console.log(sumNum);
 
@@ -80,7 +77,7 @@ console.log(sumNum);
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
-let x = 12
+let x = 12;
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
@@ -94,7 +91,7 @@ let x = 12
 
 // L'impossibilità del fatto di assegnare un nuovo valore alla variabile myName è data
 // dal fatto che il nostro terminale se chiediamo di effettuare un console.log della variabile in questione
-// riporta un errore relativo al fatto che la variabile myName era stata precedentemente dichiarata con const, 
+// riporta un errore relativo al fatto che la variabile myName era stata precedentemente dichiarata con const,
 // quindi una variabile immutabile, essendo appunto una costante!
 
 /* ESERCIZIO 6
@@ -103,10 +100,10 @@ let x = 12
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
-let sub = 4 - x // N.B. Avremmo anche potuto assegnare ad una variabile y il valore 4 ed creare la variabile sub
+let sub = 4 - x; // N.B. Avremmo anche potuto assegnare ad una variabile y il valore 4 ed creare la variabile sub
 // che avrebbe avuto come valore la sottrazione tra y ed x
 
-console.log(sub); 
+console.log(sub);
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -116,17 +113,18 @@ console.log(sub);
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
-let name1 = "john"
-let name2 = "John"
+let name1 = "john";
+let name2 = "John";
 
-console.log(name1 !== name2); 
+console.log(name1 !== name2);
 
 // In console verrà stampato il messaggio true, proprio perchè con l'operatore di comparazione !== noi stiamo verificando
 // che la disuguaglianza sia vera, ovvero che i due valori siano effettivamente diversi.
 
-(name1.toLowerCase === name2.toLowerCase) ? console.log("name1 è uguale a name2") : console.log("name1 è diverso da name2");
+name1.toLowerCase === name2.toLowerCase
+  ? console.log("name1 è uguale a name2")
+  : console.log("name1 è diverso da name2");
 
 // Con questo operatore ternario stiamo dicendo al programma che se l'uguaglianza tra la prima variabile, a cui attribuiamo un
 // lower case, e la seconda variabile, a cui attribuiamo un lowercase, risulta essere true (come ci chiedeva la traccia), allora
 // può stampare in console il messaggio "name1 è uguale a name2" altrimenti stamperà il messaggio "name1 è diverso da name2".
-
